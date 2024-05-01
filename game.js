eb1aac026ce59dd2b28ac4820e4dd3cd8e89d5e2
David <dave.j.hazelwood@gmail.com>
2024-05-01 06:35:52 -0500

    Redid the entire game logic. Everything works. Need to seperate the DOM manipulation and the game logic more. Also need to add some error handling. Should also add more comments After all that I should create a better layout and styling.

--- game.js
+++ game.js
@@ -1,130 +1,169 @@
-// Global variables
+function game() {
+    // Gameboard to be made when a new game is started
+    let board = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ];
 
-let board = [];
-let displayString = ""
-let player1 = 1
-let player2 = 2
+    // Default token for now
+    let playerToken = "X";
 
-// Starts the game. New game runs whenever you refresh the page or someone wins
-newGame();
+    // Get the gameboard element to store the tiles
+    let gameBoard = document.getElementById("gameBoard");
 
-function newGame() {
-
-    createBoard();
-    turn(player1);
-    function createBoard() {
-
-        for(let i = 0; i < 3; i++) {
-            board[i] = [];
-            for(let j = 0; j < 3; j++) {
-                board[i][j] = 0;
-            }
+    // Player object to create each player
+    function player(name, tile, score) {
+        this.name = name,
+        this.tile = tile,
+        this.score = score,
+        this.increaseScore = function() {
+            this.score += 1;
+        }
+        this.sayScores = function() {
+            console.log("Players name is: " + this.name + " their tile is " + this.tile + " their score is: " + this.score);
         }
     }
-}
 
-// Displays the game in the console
-function displayBoard() {
-    displayString = ""
-    for(let i = 0; i < 3; i++) {
-        for(let j = 0; j < 3; j++) {
-            displayString = displayString + board[i][j];
-            displayString = displayString + " | "
+    function createPlayer(token) {
+        let playerName = prompt("What is your name?");
+        if(!playerName) {
+            playerName = "Default Player";
         }
-        displayString = displayString + "\n";
+        let playerCharater = new player(playerName, token, 0);
+        return playerCharater;
     }
-    console.log(displayString);
-    if(checkWin()) {
-        console.log("Winner!");
-        newGame();
-    }
-}
 
-// Checks to make sure the space you are selecting is set to 0 (a.k.a empty)
-function validMove(xChord, yChord) {
-    if (((xChord && yChord) >= 0 && (xChord && yChord) <= 2) && (board[xChord][yChord] === 0) && !(isNaN(xChord) && isNaN(yChord))) {
-        return true;
-    }
-    return false;
-}
+    // Runs checks to see if matching numbers in rows, columns, or diagonal
+    function checkWin() {
 
-// Runs checks to see if matching numbers in rows, columns, or diagonal
-function checkWin() {
-
-    // if each row, column, daigonal direction dosen't match it returns false
-    if (checkForRow() || checkForColumn() || checkForDiagonal()) {
-        return true;
-    }
-    return false;
-
-    // Looks at each row in the board array 
-    function checkForRow() {
-        for (let row of board) {
-            const valueMatch = row[0];
-            if (row.every(val => val === valueMatch && val !== 0)) {
-                return true;
-            }
-        }
-    }
-        
-    // Looks at each column in the board array
-    function checkForColumn() {
-        for (let j = 0; j < 3; j++) {
-            const valueMatch = board[0][j];
-            if (board.every(row => row[j] === valueMatch && valueMatch !== 0)) {
-                return true;
-            }
-        }
-    }    
-
-    // Looks at the two diagonal directions the board array could match
-    function checkForDiagonal() {
-        const valueMatch = board[0][0];
-        if (valueMatch !== 0 && board[1][1] === valueMatch && board[2][2] === valueMatch) {
+        // if each row, column, daigonal direction dosen't match it returns false
+        if (checkForRow() || checkForColumn() || checkForDiagonal()) {
             return true;
         }
-        const valueMatch2 = board[0][2];
-        if (valueMatch2 !== 0 && board[1][1] === valueMatch2 && board[2][0] === valueMatch2) {
-            return true;
-        }
-    }    
-}
+        return false;
 
-// Prompts the player to pick a space to put their space. Runs itself for the other player forever
-// TODO: Make selecting the area you want to select can handle invalid or null options.
-// TODO: Make it easier to understand how to handle the user input instead of blank prompt boxes
-// TODO: Fix bug with the invalid and or error else catches. They seem to skip the players turn. Need to make and use a new fuction that
-// passes the players turn from one to the other
-function turn(player) {
-    let choiceHor = prompt(player + " enter the x chord");
-    if(!choiceHor) {
-        return;
+        // Looks at each row in the board array 
+        function checkForRow() {
+            for (let row of board) {
+                const valueMatch = row[0];
+                if (row.every(val => val === valueMatch && val !== 0)) {
+                    return true;
+                }
+            }
+        }
+            
+        // Looks at each column in the board array
+        function checkForColumn() {
+            for (let j = 0; j < 3; j++) {
+                const valueMatch = board[0][j];
+                if (board.every(row => row[j] === valueMatch && valueMatch !== 0)) {
+                    return true;
+                }
+            }
+        }    
+
+        // Looks at the two diagonal directions the board array could match
+        function checkForDiagonal() {
+            const valueMatch = board[0][0];
+            if (valueMatch !== 0 && board[1][1] === valueMatch && board[2][2] === valueMatch) {
+                return true;
+            }
+            const valueMatch2 = board[0][2];
+            if (valueMatch2 !== 0 && board[1][1] === valueMatch2 && board[2][0] === valueMatch2) {
+                return true;
+            }
+        }    
     }
-    let choiceVer = prompt(player + " enter the y chord");;
-    if(!choiceVer) {
-        return;
-    }
-    if(validMove(choiceHor, choiceVer)) {
-        try {
-            console.log("valid move");
-            board[choiceHor][choiceVer] = player;
-            displayBoard();
-            if(player === 1) {
-                turn(player2);
+
+    function createTile(xChord, yChord) {
+        // Create a new div element for the tile
+        const tile = document.createElement("div");
+        tile.className = "tile";
+        gameBoard.appendChild(tile);
+        // Add the click event listener to the tile
+        tile.addEventListener("click", () => {
+            if((xChord < 0 || xChord > 2) || (yChord < 0 || yChord > 2)) {
+                alert("You picked a spot not in bounds. Please try again");
+            }
+            else if(board[xChord][yChord] != 0) {
+                alert("You picked a spot already taken. Please try again");
             } else {
-                turn(player1);
+                switch (playerToken) {
+                    case "X":
+                        tile.classList.add("playerTokenX");
+                        break;
+                    case "O":
+                        tile.classList.add("playerTokenO");
+                        break;
+                    default:
+                        break;
+                }
+                board[xChord][yChord] = playerToken;
+                if(checkWin()) {
+                    switch (playerToken) {
+                        case "X":
+                            playerOne.increaseScore();
+                            break;
+                        case "O":
+                            playerTwo.increaseScore();
+                            break;
+                        default:
+                            break;
+                        
+                    }
+                    console.log("You win");
+                    resetGame();
+                } else {
+                    switchPlayer();
+                }
             }
-        } catch (error)  {
-            console.log("Error, please try again.");
-            turn(player);
-        }
-        
-    } else {
-        console.log("Invalid move, try again");
-        turn(player);
+        });
     }
+
+    function switchPlayer() {
+        switch (playerToken) {
+            case "X":
+                playerToken = "O";
+                break;
+            case "O":
+                playerToken = "X";
+                break;
+            default:
+                break;
+        }
+    }
+
+    function displayBoard() {
+        for (let xChord = 0; xChord < 3; xChord++) {
+            for (let yChord = 0; yChord < 3; yChord++) {
+                createTile(xChord, yChord);
+            }
+        }
+    }
+
+    function resetGame() {
+        playerToken = "X";
+        gameBoard.innerHTML = "";
+        gameBoard = document.getElementById("gameBoard");
+        for (let xChord = 0; xChord < 3; xChord++) {
+            for (let yChord = 0; yChord < 3; yChord++) {
+                board[xChord][yChord] = 0;
+            }
+        }
+        playerOne.sayScores();
+        playerTwo.sayScores();
+        displayBoard();
+
+    }
+
+    
+    // Game logic funtions. Everything past this line is what runs the game
+    displayBoard();
+    let playerOne = new createPlayer("X");
+    let playerTwo = new createPlayer("O");
+    playerOne.sayScores();
+    playerTwo.sayScores();
 }
 
-function switchPlayer() {
-    // TODO: Switch the turn order. Maybe a boolean
-}
+game();
